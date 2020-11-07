d2040bcfd7caa3be46d54f848c1612d2cec65082
SimDigital <52302068+L2Master@users.noreply.github.com>
2020-11-07 14:14:26 +0900

    Update 초대.py

--- 초대.py
+++ 초대.py
@@ -13,4 +13,4 @@ async def on_message(message):
     if message.content.startswith("초대"):
         await message.channel.send("https://discord.gg/VDQMR8e")
 
-client.run("Nzc0NDc0NjgxNTcwNjg5MDQ0.X6YTwQ.d-cbg9BRspqofs6RbqiKTMrXVeI")
+client.run("Nzc0NDc0NjgxNTcwNjg5MDQ0.X6YTwQ.-DJ-3DIB9oEhFTKQxtCsC-iC8FE")
