96f326d1d98225c0000f35e781f33246a4838a19
SimDigital <52302068+L2Master@users.noreply.github.com>
2020-11-07 21:53:44 +0900

    Update invite.py

--- invite.py
+++ invite.py
@@ -16,5 +16,5 @@ async def on_message(message):
     if message.content.startswith("초대"):
         await message.channel.send("https://discord.gg/VDQMR8e")
 
-access_token = os.environ["BOT_TOKEN"]
+access_token = os.environ['BOT_TOKEN']
 client.run("access_token")
